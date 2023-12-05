ecc81f9374e4a381c9570afd78b044bac6d88976
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2023-12-05 11:18:19 +0100

    add Day05
      - part 2 takes some time

---
+++ src/main/kotlin/de/niemeyer/aoc2023/Day05.kt
@@ -0,0 +1,115 @@
+/**
+ * Advent of Code 2023, Day 05:
+ * Problem Description: https://adventofcode.com/2023/day/05
+ */
+
+package de.niemeyer.aoc2023
+
+import de.niemeyer.aoc.utils.Resources.resourceAsText
+import de.niemeyer.aoc.utils.getClassName
+
+fun main() {
+    fun part1(input: String): Long {
+        val rules = input.split("\n\n").map { it.trim() }
+        val seeds = rules.first().substringAfter(": ").split(' ').map { it.toLong() }
+        val resources = rules.drop(1).map {
+            Resource.ofString(it)
+        }
+        val solutions = seeds.map { seed ->
+            resources.fold(seed) { acc, resource ->
+                resource.convert(acc)
+            }
+        }
+        return solutions.minOrNull() ?: error("What happened? No solution found.")
+    }
+
+    fun part2(input: String): Long {
+        val rules = input.split("\n\n").map { it.trim() }
+        val seeds = rules.first().substringAfter(": ").split(' ').map { it.toLong() }
+        val resources = rules.drop(1).map {
+            Resource.ofString(it)
+        }
+
+        var minFound = Long.MAX_VALUE
+        seeds.chunked(2)
+            .forEach { seedChunk ->
+                (seedChunk.first() until seedChunk.first() + seedChunk.last()).forEach { seed ->
+                    val solution = resources.fold(seed) { acc, resource ->
+                        resource.convert(acc)
+                    }
+                    if (solution < minFound) {
+                        minFound = solution
+                    } else {
+                        null
+                    }
+                }
+            }
+        return minFound
+    }
+
+    val name = getClassName()
+    val testInput = resourceAsText(fileName = "${name}_test.txt")
+    val puzzleInput = resourceAsText(fileName = "${name}.txt")
+
+    check(part1(testInput) == 35L)
+    val puzzleResultPart1 = part1(puzzleInput)
+    check(puzzleResultPart1 == 173_706_076L)
+
+    check(part2(testInput) == 46L)
+    val puzzleResultPart2 = part2(puzzleInput)
+    println(puzzleResultPart2)
+    check(puzzleResultPart2 == 11_611_182L)
+}
+
+enum class ISLAND_RESOURCE {
+    SEED,
+    SOIL,
+    FERTILIZER,
+    WATER,
+    LIGHT,
+    TEMPERATURE,
+    HUMIDITY,
+    LOCATION
+}
+
+data class ResourceConversion(val sourceRangeStart: Long, val destinationRangeStart: Long, val rangeLength: Long)
+
+class Resource(
+    val source: ISLAND_RESOURCE,
+    val destination: ISLAND_RESOURCE,
+    val conversions: List<ResourceConversion>
+) {
+    fun convert(startPoint: Long): Long {
+        val x = conversions.mapNotNull { conversion ->
+            if (startPoint in LongRange(
+                    conversion.sourceRangeStart,
+                    conversion.sourceRangeStart + conversion.rangeLength - 1
+                )
+            ) {
+                startPoint - conversion.sourceRangeStart + conversion.destinationRangeStart
+            } else {
+                null
+            }
+        }.firstOrNull()
+        return x ?: startPoint
+    }
+
+    companion object {
+        fun ofString(input: String): Resource {
+            val lines = input.lines()
+            val (sourceText, _, destinationText) = lines.first().split('-', ' ')
+            val conversionList = lines.drop(1).map { line ->
+                val (destinationStart, sourceStart, rangeLength) = line.split(' ').map { num ->
+                    num.toLong()
+                }
+                ResourceConversion(sourceStart, destinationStart, rangeLength)
+            }
+            return Resource(
+                enumValueOf<ISLAND_RESOURCE>(sourceText.uppercase()),
+                enumValueOf<ISLAND_RESOURCE>(destinationText.uppercase()),
+                conversionList
+            )
+        }
+    }
+}
+
