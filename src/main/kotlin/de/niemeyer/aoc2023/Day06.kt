b9755550f8bc7bb05438e8e8cb233314ed292064
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2023-12-06 17:42:33 +0100

    refactor Day 6

--- src/main/kotlin/de/niemeyer/aoc2023/Day06.kt
+++ src/main/kotlin/de/niemeyer/aoc2023/Day06.kt
@@ -8,50 +8,41 @@ package de.niemeyer.aoc2023
 import de.niemeyer.aoc.utils.Resources.resourceAsList
 import de.niemeyer.aoc.utils.executeAndCheck
 import de.niemeyer.aoc.utils.getClassName
+import de.niemeyer.aoc.utils.product
 
 fun main() {
-    fun part1(input: List<String>): Long {
-        val x = input.map { line ->
-            line.split(" ").drop(1).filter { it.isNotBlank() }.map { it.toLong() }
-        }
-        val races = x.first().zip(x.last())
-        val s = races.map { race ->
-            (1 until race.first).map { duration ->
-                duration * (race.first - duration)
-            }.filter { solution ->
-                solution > race.second
-            }.count()
-        }.fold(1L) { acc, i ->
-            acc * i
-        }
-        return s
-    }
+    fun part1(input: List<String>): Int =
+        input.map { line ->
+            line.split(" ").drop(1).filter { it.isNotBlank() }.map { it.toInt() }
+        }.let { (times, durations) ->
+            times.zip(durations)
+        }.map { (time, duration) ->
+            (1 until time).map { d ->
+                d * (time - d)
+            }.count { it > duration }
+        }.product()
 
-    fun part2(input: List<String>): Long {
-        val x = input.map { line ->
+    fun part2(input: List<String>): Int {
+        val (time, duration) = input.map { line ->
             line.substringAfter(":").replace(" ", "").toLong()
-        }
-        val race = x.first() to x.last()
-        val s = (1L until race.first).map { duration ->
-            duration * (race.first - duration)
-        }.filter { solution ->
-            solution > race.second
-        }.count().toLong()
+        }.let { it.first() to it.last() }
 
-        return s
+        return (1 until time).map { d ->
+            d * (time - d)
+        }.count { it > duration }
     }
 
     val name = getClassName()
     val testInput = resourceAsList(fileName = "${name}_test.txt")
     val puzzleInput = resourceAsList(fileName = "${name}.txt")
 
-    check(part1(testInput) == 288L)
-    executeAndCheck(1, 1_159_152L) {
+    check(part1(testInput) == 288)
+    executeAndCheck(1, 1_159_152) {
         part1(puzzleInput)
     }
 
-    check(part2(testInput) == 71_503L)
-    executeAndCheck(2, 41_513_103L) {
+    check(part2(testInput) == 71_503)
+    executeAndCheck(2, 41_513_103) {
         part2(puzzleInput)
     }
 }
