6a736c93b8d486a5f5db8c96254a6937980de94e
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2023-12-18 13:35:03 +0100

    add Day 18

--- src/main/kotlin/de/niemeyer/aoc2023/Day18.kt
+++ src/main/kotlin/de/niemeyer/aoc2023/Day18.kt
@@ -1,6 +1,7 @@
 /**
- * Advent of Code 2023, Day 18:
+ * Advent of Code 2023, Day 18: Lavaduct Lagoon
  * Problem Description: https://adventofcode.com/2023/day/18
+ * Disclaimer: The solution is from Michael de Kaste w/ minimal refactoring from my side.
  */
 
 package de.niemeyer.aoc2023
@@ -10,60 +11,25 @@ import de.niemeyer.aoc.direction.toDirectionScreen
 import de.niemeyer.aoc.points.Point2D
 import de.niemeyer.aoc.utils.Resources.resourceAsList
 import de.niemeyer.aoc.utils.*
+import kotlin.math.absoluteValue
+
+fun List<Pair<DirectionScreen, Int>>.solve() =
+    runningFold(Point2D.ORIGIN) { point, (dir, amount) ->
+        point.moveTimes(dir, amount)
+    }.zipWithNext { p1, p2 ->
+        (p2.x - p1.x) * p1.y.toLong()
+    }.sum().absoluteValue + sumOf { it.second } / 2 + 1
+
+private fun String.toDigLine(): Pair<DirectionScreen, Int> {
+    val dir = listOf(DirectionScreen.Right, DirectionScreen.Down, DirectionScreen.Left, DirectionScreen.Up)[get(lastIndex - 1).digitToInt()]
+    return dir to this.substring(2..6).toInt(16)
+}
 
 fun main() {
-    fun solve(input: List<String>): Long {
-        val instructions = input.map { Dig.of(it) }
+    fun part1(input: List<String>): Long =
+        input.map { it.split(' ') }.map { (a, b, _) -> a[0].toDirectionScreen() to b.toInt() }.solve()
 
-        val plan = buildMap<Point2D, Pair<DirectionScreen, DirectionScreen>> {
-            var current = Point2D.ORIGIN
-            this[current] = DirectionScreen.Right to DirectionScreen.Right
-            instructions.forEach { dig ->
-                repeat(dig.distance) {
-                    this[current] = this[current]!!.first to dig.direction
-                    current = current.move(dig.direction)
-                    this[current] = dig.direction to (this[current]?.second ?: dig.direction)
-                }
-            }
-        }
-        var interior = 0L
-        val rowMin = plan.keys.minOf { it.y }
-        val rowMax = plan.keys.maxOf { it.y }
-        val columnMin = plan.keys.minOf { it.x }
-        val columnMax = plan.keys.maxOf { it.x }
-        val verticalDirs = listOf(DirectionScreen.Up, DirectionScreen.Down)
-        for (row in rowMin..rowMax) {
-            var inside = false
-            var firstDig: Pair<DirectionScreen, DirectionScreen>? = null
-            var lastDig: Pair<DirectionScreen, DirectionScreen>? = null
-            for (column in columnMin..columnMax) {
-                val point = Point2D(column, row)
-                val dig = plan[point]
-                if (lastDig == null && dig != null) {
-                    firstDig = dig
-                } else if (lastDig != null && dig == null) {
-                    if (firstDig!!.first == lastDig.second && lastDig.second in verticalDirs
-                    ) {
-                        inside = !inside
-                    } else if (firstDig!!.second == lastDig.first && lastDig.first in verticalDirs
-                    ) {
-                        inside = !inside
-                    }
-                }
-                lastDig = dig
-                if (dig != null || inside) {
-                    print(if (dig != null) '#' else 'O')
-                    interior++
-                } else print('.')
-            }
-            println()
-        }
-        return interior
-    }
-
-    fun part1(input: List<String>): Long = solve(input)
-
-    fun part2(input: List<String>): Long = TODO()
+    fun part2(input: List<String>): Long = input.map { it.split(' ') }.map { (_, _, c) -> c.toDigLine() }.solve()
 
     val name = getClassName()
     val testInput = resourceAsList(fileName = "${name}_test.txt")
@@ -74,20 +40,8 @@ fun main() {
         part1(puzzleInput)
     }
 
-//    check(part2(testInput) == 952_408_144_115L)
-//    executeAndCheck(2, 0L) {
-//        part2(puzzleInput)
-//    }
-}
-
-data class Dig(val direction: DirectionScreen, val distance: Int, val rgb: String) {
-    companion object {
-        fun of(input: String): Dig {
-            val parts = input.split(' ')
-            val direction = parts.first().toDirectionScreen()
-            val distance = parts[1].toInt()
-            val rgb = parts.last().substringAfter('(').substringBefore(')')
-            return Dig(direction, distance, rgb)
-        }
+    check(part2(testInput) == 952_408_144_115L)
+    executeAndCheck(2, 44_644_464_596_918L) {
+        part2(puzzleInput)
     }
 }
