eed9ba86880e191b883a6af332b29a8c84e19a8e
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2023-12-23 11:25:07 +0100

    refactor Day 22

--- src/main/kotlin/de/niemeyer/aoc2023/Day22.kt
+++ src/main/kotlin/de/niemeyer/aoc2023/Day22.kt
@@ -1,7 +1,6 @@
 /**
  * Advent of Code 2023, Day 22: Sand Slabs
  * Problem Description: https://adventofcode.com/2023/day/22
- * Disclaimer: Code by to Roman Elizarov w/ little refactoring
  */
 
 package de.niemeyer.aoc2023
@@ -10,15 +9,13 @@ import de.niemeyer.aoc.points.Point2D
 import de.niemeyer.aoc.points.Point3D
 import de.niemeyer.aoc.utils.Resources.resourceAsList
 import de.niemeyer.aoc.utils.*
-import kotlin.math.abs
-import kotlin.math.sign
 
 fun main() {
     fun part1(input: List<String>): Int =
-        Day22(input).solve1()
+        Day22(input).solve()
 
     fun part2(input: List<String>): Int =
-        Day22(input).solve2()
+        Day22(input).solve(chainReaction = true)
 
     val name = getClassName()
     val testInput = resourceAsList(fileName = "${name}_test.txt")
@@ -36,81 +33,56 @@ fun main() {
 }
 
 class Day22() {
-    var bricks: List<Brick> = emptyList()
+    private var bricks = emptyList<Brick>()
 
     constructor(input: List<String>) : this() {
-        bricks = input.mapIndexed { idx, line ->
+        bricks = input.map { line ->
             Brick.of(line)
         }.sortedBy { it.a.z }
     }
 
-    fun solve1(): Int {
-        val numBricks = bricks.size
-        val frameBuffer = Array(10) { IntArray(10) }
-        val zBuffer = Array(10) { IntArray(10) { -1 } }
-        val disintegratable = BooleanArray(numBricks) { true }
-        for ((brickIdx, brick) in bricks.withIndex()) {
-            val dx = (brick.b.x - brick.a.x).sign
-            val dy = (brick.b.y - brick.a.y).sign
-            val xyExpansion = IntRange(0, abs(brick.a.x - brick.b.x) + abs(brick.a.y - brick.b.y))
-            val sz = xyExpansion.fold(0) { acc, i ->
-                val p = Point2D(brick.a.x + i * dx, brick.a.y + i * dy)
+    fun solve(chainReaction: Boolean = false): Int {
+        val maxDim = bricks.maxOf { maxOf(it.a.x, it.a.y, it.b.x, it.b.y) } + 1
+        val infinity = -1
+        val zBuffer = Array(maxDim) { IntArray(maxDim) { infinity } }
+        val frameBuffer = Array(maxDim) { IntArray(maxDim) }
+        val disintegratable = BooleanArray(bricks.size) { true }
+        val supportChain = Array(bricks.size) { HashSet<Int>() }
+        bricks.forEachIndexed { brickIdx, brick ->
+            val xyLayer = brick.xyLayer()
+
+            val highestZInLayer = xyLayer.fold(0) { acc, p ->
                 maxOf(acc, frameBuffer[p])
             }
 
             val support = HashSet<Int>()
-            xyExpansion.forEach { i ->
-                val p = Point2D(brick.a.x + i * dx,brick.a.y + i * dy)
-                if (sz == frameBuffer[p] && zBuffer[p] >= 0) {
-                    support += zBuffer[p]
+            xyLayer.forEach {
+                if (highestZInLayer == frameBuffer[it]) {
+                    supportChain[brickIdx] += zBuffer[it]
+                    if (zBuffer[it] > infinity) {
+                        support += zBuffer[it]
+                    }
                 }
             }
-            if (support.size == 1) disintegratable[support.single()] = false
-            val nz = sz + 1 + brick.b.z - brick.a.z
-            xyExpansion.forEach { i ->
-                val p = Point2D(brick.a.x + i * dx, brick.a.y + i * dy)
-                frameBuffer[p] = nz
-                zBuffer[p] = brickIdx
+            val nextZ = highestZInLayer + 1 + brick.b.z - brick.a.z
+            xyLayer.forEach {
+                frameBuffer[it] = nextZ
+                zBuffer[it] = brickIdx
+            }
+            if (support.size == 1) {
+                disintegratable[support.single()] = false
             }
         }
-        return disintegratable.count { it }
-    }
-
-    fun solve2(): Int {
-        val numBricks = bricks.size
-        val frameBuffer = Array(10) { IntArray(10) }
-        val zBuffer = Array(10) { IntArray(10) { -1 } }
-        val support = Array(numBricks) {HashSet<Int>() }
-        for ((brickIdx, brick) in bricks.withIndex()) {
-            val dx = (brick.b.x - brick.a.x).sign
-            val dy = (brick.b.y - brick.a.y).sign
-            val xyExpansion = IntRange(0, abs(brick.a.x - brick.b.x) + abs(brick.a.y - brick.b.y))
-            val sz = xyExpansion.fold(0) { acc, i ->
-                val p = Point2D(brick.a.x + i * dx, brick.a.y + i * dy)
-                maxOf(acc, frameBuffer[p])
-            }
-
-            xyExpansion.forEach { i ->
-                val p = Point2D(brick.a.x + i * dx,brick.a.y + i * dy)
-                if (sz == frameBuffer[p]) {
-                    support[brickIdx] += zBuffer[p]
-                }
-            }
-            val nz = sz + 1 + brick.b.z - brick.a.z
-            xyExpansion.forEach { i ->
-                val p = Point2D(brick.a.x + i * dx, brick.a.y + i * dy)
-                frameBuffer[p] = nz
-                zBuffer[p] = brickIdx
-            }
-        }
+        if (!chainReaction)
+            return disintegratable.count { it }
 
         var result = 0
-        for (ti in 0..<numBricks) {
+        bricks.indices.forEach { brickIdx ->
             val ds = HashSet<Int>()
-            ds += ti
+            ds += brickIdx
             do {
                 var changes = false
-                for (tj in 0..<numBricks) if (tj !in ds && (support[tj] - ds).isEmpty()) {
+                for (tj in bricks.indices) if (tj !in ds && (supportChain[tj] - ds).isEmpty()) {
                     ds += tj
                     changes = true
                 }
@@ -120,6 +92,9 @@ class Day22() {
         return result
     }
 }
+
+fun Brick.xyLayer(): List<Point2D> =
+    Point2D(a.x, a.y).lineTo(Point2D(b.x, b.y))
 
 data class Brick(val a: Point3D, val b: Point3D) {
     companion object {
